4cc73112497c8f062c29ee336eaa619f2245b9cc
LunguRadu <lungu.radu123@gmail.com>
2022-02-17 13:31:55 -0600

    changed app run structure

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def get_class(class_index):
 
 
 if __name__ == '__main__':
-    Flask.run(app)
+    app.run()
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
