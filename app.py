937cafbfb3fcb3b675c2317294814708e87c78cf
vichy <vmeas@macalester.edu>
2022-02-22 15:14:03 -0600

    add more model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import base64
 import json
+from unicodedata import name
 import numpy as np
 import io
 import re
@@ -16,14 +17,32 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
+"""
+For deployment: 
+change the CORS policy below to heroku frontend only 
+"""
+## For produciton 
+# CORS(app, origins=["https://cs484-cnn-classifier.herokuapp.com/:7800"])
+## For development 
 CORS(app)
 
 
 # Function to get the model
 def get_model():
-    global model
-    model = load_model('fine_tuning_vgg16_1.h5')
+    # global model
+    # model = load_model('fine_tuning_vgg16_1.h5')
     # model = load_model('autovgg15model.h5')
+    global fine_tuning_vgg16_1_model
+    global fine_tuning_vgg16_2_model
+    global vgg_structure_model
+    global vgg16_2_model
+    global vgg16_model
+    fine_tuning_vgg16_1_model = load_model('fine_tuning_vgg16_1.h5')
+    fine_tuning_vgg16_2_model = load_model('fine_tuning_vgg16_2.h5')
+    vgg_structure_model = load_model('vgg16_structure.h5')
+    vgg16_2_model = load_model('vgg16_2.h5')
+    vgg16_model = load_model('vgg16.h5')
+
     print(" * Model loaded!")
 
 
@@ -61,26 +80,43 @@ def predict():
     image_data = re.sub('^data:image/.+;base64,', '', encoded)
     decoded = base64.b64decode(image_data)
     image = Image.open(io.BytesIO(decoded))
+    #  Preprocess image for Models
     processed_image = preprocess_image(image, target_size=(224, 224))
-    prediction = model.predict(processed_image)[0]
-    top_5_indices = (-prediction).argsort()[:NUMBER_PREDICTION]
-    prediction_confidence = get_prediction_confidence(top_5_indices, prediction, NUMBER_PREDICTION)
-    response = {"prediction":str(prediction_confidence)}
-    return json.dumps(response)
+    # Model order array
+    MODEL_NAME =  [ 'fine_tuning_vgg16_1_model','fine_tuning_vgg16_2_model', 'vgg_structure_model', 'vgg16_2_model', 'vgg16_model']
+    response = dict()
+    for name, model in zip(MODEL_NAME, [fine_tuning_vgg16_1_model,fine_tuning_vgg16_2_model, vgg_structure_model, vgg16_2_model, vgg16_model]):
+        response[name] = getPrediction(model,processed_image, NUMBER_PREDICTION)
+    print(response)
+    print(jsonify(response))
+    print(json.dumps(response))
+    return jsonify(response)
 
 def get_prediction_confidence(class_indices, prediction_percentage, NUMBER_PREDICTION):
     with open('bodyType_classes.json') as json_file:
         classes_dic = json.load(json_file)
         result = dict()
+        order = []
         for index in range(NUMBER_PREDICTION): # 0 --> 5 top 5 prediction
             class_index = class_indices[index]
-            result[classes_dic[str(class_index)]] = prediction_percentage[class_index]
-        return result
+            class_name = classes_dic[str(class_index)]
+            result[class_name] = str(prediction_percentage[class_index])
+            order.append(class_name)
+        return result, order
 
-def get_class(class_index):
-    with open('bodyType_classes.json') as json_file:
-        classes_dic = json.load(json_file)
-        return classes_dic[str(class_index)]
+def getPrediction(model,  preprocessed_image, number_top_prediction):
+    #  get prediction from model
+    prediction = model.predict(preprocessed_image)[0]
+    #  get top indice associate to classification
+    top_indices = (-prediction).argsort()[:number_top_prediction]
+    #  map class and prediction confidence 
+    prediction_confidence, classification_order = get_prediction_confidence(top_indices, prediction, number_top_prediction)
+    return {"confidence":prediction_confidence, "order": classification_order}
+
+# def get_class(class_index):
+#     with open('bodyType_classes.json') as json_file:
+#         classes_dic = json.load(json_file)
+#         return classes_dic[str(class_index)]
 
 
 if __name__ == '__main__':
